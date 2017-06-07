c03957403f0d3baa7b231874b9b404760d333314
Alessio Ferri <alessio.ferri.3012@gmail.com>
2017-06-07 18:25:44 +0200

    Inizio modello

---
+++ Signum/Signum/Model/IElemento.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Signum.Model
+{
+    interface IElemento
+    {
+
+        int Durata { get; }
+
+        Informazione InfomazioneAssociata { get; }
+
+    }
+}
---
+++ Signum/Signum/Model/Informazione.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Signum.Model
+{
+    abstract class Informazione
+    {
+
+        private string _value;
+
+        public Informazione(string value)
+        {
+            _value = value;
+        }
+
+        public virtual string Value {
+            get {
+                return _value;
+            }
+            protected set
+            {
+                _value = Value;
+            }
+        }
+
+    }
+}
