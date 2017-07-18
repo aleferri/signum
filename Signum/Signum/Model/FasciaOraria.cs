86e942bd3fc9726b30aac48a42860f8012554a73
alessio <alessio.ferri.3012@gmail.com>
2017-07-18 18:12:52 +0200

    Aggiunti FasciaOraria.cs e messo a posto la ProgrammazioneGiornaliera
    TODO: trim to fit e ripulire lista dummies ogni tanto
    
    Signed-off-by: alessio <alessio.ferri.3012@gmail.com>

---
+++ Signum/Signum/Model/FasciaOraria.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Signum.Model
+{
+    public class FasciaOraria
+    {
+
+        private readonly uint _startQuarter;
+        private readonly uint _endQuarter;
+
+        public FasciaOraria(uint startQuarter, uint endQuarter)
+        {
+            _startQuarter = startQuarter;
+            _endQuarter = endQuarter;
+        }
+
+        public uint StartQuarter => _startQuarter;
+
+        public uint EndQuarter => _endQuarter;
+
+    }
+}
--- Signum/Signum/Model/ProgrammazioneGiornaliera.cs
+++ Signum/Signum/Model/ProgrammazioneGiornaliera.cs
@@ -11,9 +11,11 @@ namespace Signum.Model
     public class ProgrammazioneGiornaliera : ICopiable<ProgrammazioneGiornaliera>
     {
 
+        public static readonly uint QUARTERS_IN_DAY = 4 * 24;
+        public static readonly uint QUARTER_DURATION = 15 * 60;
         public static readonly uint SECONDS_IN_DAY = 60 * 60 * 24;
 
-        private readonly IList<Sequenza> _sequences;
+        private readonly Sequenza[] _sequences;
         private readonly IList<Sequenza> _dummies;
         private string _nome;
 
@@ -26,108 +28,91 @@ namespace Signum.Model
 
         public ProgrammazioneGiornaliera()
         {
-            _sequences = new List<Sequenza>();
+            _sequences = new Sequenza[96];
             _dummies = new List<Sequenza>();
             Sequenza dummy = new Sequenza();
             dummy.AggiungiElemento(ElementoDummy.DUMMY, Sequenza.MAX_DURATION);
             _dummies.Add(dummy);
-            _sequences.Add(dummy);
-        }
-
-        private int FindNearDummy(int index, out Sequenza dummy)
-        {
-            if (index > 0)
+            for (int i = 0; i < _sequences.Length; i++)
             {
-                if (_dummies.Contains(_sequences[index - 1]))
-                {
-                    dummy = _sequences[index - 1];
-                    return index - 1;
-                }
-            }
-            if (index < _sequences.Count - 1)
-            {
-                if (_dummies.Contains(_sequences[index + 1]))
-                {
-                    dummy = _sequences[index + 1];
-                    return index + 1;
-                }
-            }
-            dummy = null;
-            return -1;
-        }
-
-        private int FindDummy(uint durata, out Sequenza dummy)
-        {
-            IEnumerable<Sequenza> seqs = from Sequenza s in _sequences
-                                         orderby Math.Abs(s.Durata - durata)
-                                         select s;
-            dummy = seqs.Count() > 0 ? seqs.First() : null;
-            if (dummy == null)
-            {
-                return -1;
-            }
-            return _sequences.IndexOf(dummy);
-        }
-
-        private void MergeDummies(int startIndex)
-        {
-            if (startIndex < _sequences.Count)
-            {
-                return;
-            }
-            Sequenza a = _sequences[startIndex];
-            Sequenza b = _sequences[startIndex + 1];
-            if (_dummies.Contains(a) && _dummies.Contains(b))
-            {
-                Sequenza dummy = new Sequenza();
-                dummy.AggiungiElemento(ElementoDummy.DUMMY, a.Durata + b.Durata);
-                _dummies.Remove(a);
-                _dummies.Remove(b);
-                _sequences[startIndex] = dummy;
-                _sequences.Remove(b);
+                _sequences[i] = dummy;
             }
         }
 
         public void Remove(Sequenza s)
         {
-            Sequenza dummy;
-            int indexOf = FindNearDummy(_sequences.IndexOf(s), out dummy);
-            _sequences.Remove(s);
-            if (dummy != null)
+            Sequenza dummy = null;
+            for (int i = 0; i < _sequences.Length; i++)
             {
-                dummy.AggiungiElemento(ElementoDummy.DUMMY, s.Durata);
-                MergeDummies(indexOf);
-            }
-            else
-            {
-                Sequenza dummyReplace = new Sequenza();
-                dummyReplace.AggiungiElemento(ElementoDummy.DUMMY, s.Durata);
+                if (_sequences[i] == s)
+                {
+                    if (dummy == null)
+                    {
+                        dummy = new Sequenza();
+                        _dummies.Add(dummy);
+                    }
+                    dummy.AggiungiElemento(ElementoDummy.DUMMY, QUARTER_DURATION);
+                    _sequences[i] = dummy;
+                }
+                else
+                {
+                    dummy = _dummies.Contains(_sequences[i]) ? _sequences[i] : null;
+                }
             }
         }
 
         public bool AggiungiSequenza(Sequenza s)
         {
-            int indexOf = FindDummy(s.Durata, out Sequenza dummy);
-            if (indexOf < 0)
+            int required = s.Count;
+            uint start = 0;
+            int count = 0;
+            bool accepted = false;
+            for (uint i = 0; i < _sequences.Length; i++)
             {
-                throw new ArgumentException("Impossibile insere, nessuno spazio libero");
+                if (_dummies.Contains(_sequences[i]))
+                {
+                    count++;
+                    if (count == required)
+                    {
+                        accepted = true;
+                        break;
+                    }
+                }
+                else
+                {
+                    count = 0;
+                    start = i + 1;
+                }
             }
-            bool trimmed = _sequences[indexOf].Durata < s.Durata;
-            uint fitDurata = trimmed
-                    ? _sequences[indexOf].Durata
-                    : s.Durata;
-            uint durationLeft = _sequences[indexOf].Durata - fitDurata;
-            _sequences.Remove(dummy);
-            _dummies.Remove(dummy);
-            if (durationLeft > 0)
+            if (accepted)
             {
-                Sequenza cDummy = new Sequenza();
-                cDummy.AggiungiElemento(ElementoDummy.DUMMY, durationLeft);
-                _dummies.Add(cDummy);
-                _sequences.Insert(indexOf, cDummy);
+                for (uint i = start; i < start + count; i++)
+                {
+                    _sequences[i] = s;
+                }
             }
-            _sequences.Insert(indexOf, s);
-            return !trimmed;
+            return accepted;
+        }
+
+        public bool InserisciSequenza(Sequenza s, FasciaOraria f)
+        {
+            if (!_dummies.Contains(_sequences[f.StartQuarter]))
+            {
+                return false;
+            }
+            bool accepted = true;
+            for (uint i = f.StartQuarter; i < f.EndQuarter + 1; i++)
+            {
+                accepted &= _dummies.Contains(_sequences[i]);
+            }
+            if (accepted)
+            {
+                for (uint i = f.StartQuarter; i < f.EndQuarter + 1; i++)
+                {
+                    _sequences[i] = s;
+                }
+            }
+            return accepted;
         }
 
         public ProgrammazioneGiornaliera Copy()
